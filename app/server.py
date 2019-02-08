59d0c5b053c69f1b2f24d5158c2279dcc54afe69
ievans-HMC <47442157+ievans-HMC@users.noreply.github.com>
2019-02-07 23:27:51 -0800

    Trying new export.pkl file on google drive

--- app/server.py
+++ app/server.py
@@ -8,9 +8,8 @@ from io import BytesIO
 from fastai import *
 from fastai.vision import *
 
-export_file_url = 'https://drive.google.com/uc?export=download&id=14RXjQBoE5rGZ4YlFi_75nMVL-YKu8SQl'
-#Our learner file
-#'https://drive.google.com/uc?export=download&id=1jx9XrqgvGcKLqItftlGAPLnhnhSVPtW_'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1NB5OwPb34_q8jlyti3Sa_Hr47WuaIGW3'
+
 export_file_name = 'export.pkl'
 
 classes = ['black', 'grizzly', 'teddys']
