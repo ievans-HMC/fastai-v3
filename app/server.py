5c349ee91ebe0cd2769c3e2ebc96ef5d391da75c
ievans-HMC <47442157+ievans-HMC@users.noreply.github.com>
2019-02-07 23:02:18 -0800

    Testing with provided learner

--- app/server.py
+++ app/server.py
@@ -8,7 +8,9 @@ from io import BytesIO
 from fastai import *
 from fastai.vision import *
 
-export_file_url = 'https://drive.google.com/uc?export=download&id=1jx9XrqgvGcKLqItftlGAPLnhnhSVPtW_'
+export_file_url = 'https://drive.google.com/uc?export=download&id=14RXjQBoE5rGZ4YlFi_75nMVL-YKu8SQl'
+#Our learner file
+#'https://drive.google.com/uc?export=download&id=1jx9XrqgvGcKLqItftlGAPLnhnhSVPtW_'
 export_file_name = 'export.pkl'
 
 classes = ['black', 'grizzly', 'teddys']
